5e47e201c45d28c0930cca8dbb11630579ea5b56
zgsxxzzyl@hotmail.com <zgsxxzzyl@hotmail.com>
2020-03-16 18:04:46 +0800

    20200303

---
+++ src/main/java/strings/Solution779.java
@@ -0,0 +1,9 @@
+package strings;
+
+class Solution779 {
+    public String addBinary(String a, String b) {
+        char[] aa = a.toCharArray();
+        char[] bb = b.toCharArray();
+        return null;
+    }
+}
