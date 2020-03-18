595bc8c6a57ebf60d14221117ae6fbc2bcb6cd11
zgsxxzzyl@hotmail.com <zgsxxzzyl@hotmail.com>
2020-03-18 16:58:41 +0800

    20200318

--- src/main/java/strings/Solution779.java
+++ src/main/java/strings/Solution779.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.math.BigInteger;
+
 /**
  * https://leetcode-cn.com/problems/add-binary/solution/er-jin-zhi-qiu-he-by-leetcode/
  */
@@ -9,6 +11,48 @@ class Solution779 {
     }
 
     public static void main(String[] args) {
+        String a = "1111";
+        String b = "10";
+        System.out.println(new Solution779().addBinaryExt(a, b));
+    }
 
+    public String addBinaryExt(String a, String b) {
+        int n = a.length(), m = b.length();
+        if (n < m) {
+            return addBinaryExt(b, a);
+        }
+        int l = Math.max(n, m);
+        StringBuilder sb = new StringBuilder();
+        int carry = 0, j = m - 1;
+        for (int i = l - 1; i > -1; --i) {
+            if (a.charAt(i) == '1') {
+                ++carry;
+            }
+            if (j > -1 && b.charAt(j--) == '1') {
+                ++carry;
+            }
+            if (carry % 2 == 1) {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+            carry = carry / 2;
+        }
+        if (carry == 1) sb.append('1');
+        return sb.reverse().toString();
+    }
+
+    public String addBinaryExt2(String a, String b) {
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger zero = new BigInteger("0", 2);
+        BigInteger carry, answer;
+        while (y.compareTo(zero) != 0) {
+            answer = x.xor(y);
+            carry = x.and(y).shiftLeft(1);
+            x = answer;
+            y = carry;
+        }
+        return x.toString(2);
     }
 }
