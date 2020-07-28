b8e74245fc4214a18f2310864969c04f39c8f998
zgsxxzzyl@hotmail.com <zgsxxzzyl@hotmail.com>
2020-07-28 16:04:51 +0800

    20200728

--- src/main/java/strings/Solution36.java
+++ src/main/java/strings/Solution36.java
@@ -1,5 +1,10 @@
 package strings;
 
+import linkedlist.ListNode;
+
+/**
+ * 判断是否是回路字符串
+ */
 class Solution36 {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
@@ -45,5 +50,43 @@ class Solution36 {
         String s = "";
         boolean palindrome = new Solution36().isPalindromeBest(s);
         System.out.println(palindrome);
+    }
+
+    /**
+     * 使用快慢两个指针找到链表中点，慢指针每次前进一步，快指针每次前进两步。在慢指针前进的过程中，同时修改其 next 指针，使得链表前半部分反序。最后比较中点两侧的链表是否相等。
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (slow != null) {
+            if (slow.val != prev.val) {
+                return false;
+            }
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        return true;
     }
 }
